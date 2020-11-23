c03e9636fb553bd5cca53ed4ceafc6d649039be6
Thierry Laporte <laportethierry@yahoo.fr>
2020-11-23 19:31:56 +0100

    index cards sidebar logo-navbar

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -1,11 +1,11 @@
 class CompetitorsController < ApplicationController
   def index
     @competitors = Competitor.all
+    @user = User.new
   end
 
   def show
     @competitor = Competitor.find(params[:id])
   end
-
-  def 
+ 
 end
