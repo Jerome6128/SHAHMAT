f1473726826c57322e33d5a76aa4202d6f670cbb
Thierry Laporte <laportethierry@yahoo.fr>
2020-11-24 18:26:43 +0100

    index and show

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,3 +1,10 @@
 class Competitor < ApplicationRecord
   belongs_to :user
+
+  include PgSearch::Model
+  pg_search_scope :search_by_brand_name_and_siren,
+    against: [ :brand_name, :siren ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
