d13364efd285beb3137cac78a9c93662baad0eee
Jerome Gratelle <jerome.gratelle@air-indemnite.com>
2020-11-29 12:34:01 +0100

    seeed aveec legal tech

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :competitors
+  has_many :competitors, dependent: :destroy
 end
