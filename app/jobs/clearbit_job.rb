6177a5507f5555a548189d4c4e5d549ce48aef85
Jerome Gratelle <jerome.gratelle@air-indemnite.com>
2020-12-03 13:01:15 +0100

    correction bugs si plusieurs résultats

--- app/jobs/clearbit_job.rb
+++ app/jobs/clearbit_job.rb
@@ -6,12 +6,13 @@ class ClearbitJob < ApplicationJob
     url = "https://autocomplete.clearbit.com/v1/companies/suggest?query=#{competitor.trading_name.encode("ASCII", "UTF-8", undef: :replace)}"
     competitor_autocomplete = HTTParty.get(url)
     if competitor_autocomplete.size == 1
-      competitor_autocomplete = competitor_autocomplete.first
+      competitor_select = competitor_autocomplete.first
     else
-      competitor_autocomplete = competitor_autocomplete.select { |item| item["name"].capitalize == competitor.trading_name.capitalize }
+      competitor_select = competitor_autocomplete.select { |item| item["name"].capitalize == competitor.trading_name.capitalize }
+      competitor_select = competitor_select.first
     end
-    competitor.website = "https://www.#{competitor_autocomplete['domain']}"
-    file = URI.open(competitor_autocomplete['logo'])
+    competitor.website = "https://www.#{competitor_select['domain']}"
+    file = URI.open(competitor_select['logo'])
     competitor.photo.attach(io: file, filename: competitor.trading_name, content_type: "image/png")
 
     competitor.save
